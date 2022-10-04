d3e8b7c51bb6e848d4db5c98f8b97b1f3f70a4bf
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 06:59:44 +0000

    Adding numeric_owner as keyword arguement

--- Training/models-1.12.0/research/deep_speech/data/download.py
+++ Training/models-1.12.0/research/deep_speech/data/download.py
@@ -94,7 +94,7 @@ def download_and_extract(directory, url):
               if not is_within_directory(path, member_path):
                   raise Exception("Attempted Path Traversal in Tar File")
       
-          tar.extractall(path, members, numeric_owner) 
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
           
       
       safe_extract(tar, directory)
